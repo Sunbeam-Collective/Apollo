76a832794700a480dc6ecbc49b3aa060497cb7ef
Jordit <jordidiaz2020@gmail.com>
2025-03-17 19:26:58 -0400

    Fix Routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 import {
   Playground,
@@ -10,24 +11,23 @@ import {
   Home,
   Mixer,
   Player,
-  Redirect
-} from './pages'
+  Redirect,
+} from "./pages";
+
 function App() {
   return (
     <>
-      <div>
-        <Routes>
-          <Route path='/playground' element={<Playground />} />
-          <Route path='/' element={<Redirect />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/mixer/:id' element={<Mixer />} />
-          <Route path='/player/:id' element={<Player />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/playground" element={<Playground />} />
+        <Route path="/" element={<Redirect />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/mixer/:id" element={<Mixer />} />
+        <Route path="/player/:id" element={<Player />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
